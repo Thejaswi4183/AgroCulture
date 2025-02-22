1110b1d0ef494d9d31086f6db38d1ab62f23a9e5
Thejaswi S <thejaswi4uns@gmail.com>
2025-02-22 10:57:26 +0530

    Created footer.php

---
+++ footer.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<head>
+<link rel="stylesheet" type="text/css" href="indexFooter.css">
+</head>
+<body>
+<!-- Footer -->
+<footer class="footer-distributed" style="background-color:black" id="aboutUs">
+		<center>
+			<h1 style="font: 35px calibri;">About Us</h1>
+		</center>
+		<div class="footer-left">
+			<h3 style="font-family: 'Times New Roman', cursive;">AgroCulture &copy; </h3>
+			<br />
+			<p style="font-size:20px;color:white">Your product Our market !!!</p>
+			<br />
+		</div>
+
+		<div class="footer-center">
+			<div>
+				<i class="fa fa-map-marker"></i>
+				<p style="font-size:20px">Agro Culture Fam</p>
+			</div>
+			<div>
+				<i class="fa fa-phone"></i>
+				<p style="font-size:20px">9731859761</p>
+			</div>
+			<div>
+				<i class="fa fa-envelope"></i>
+				<p style="font-size:20px"><a href="mailto:agroculture@gmail.com"
+						style="color:white">thejaswi4uns@gmail.com</a></p>
+			</div>
+		</div>
+
+		<div class="footer-right">
+			<p class="footer-company-about" style="color:white">
+				<span style="font-size:20px"><b>About AgroCulture</b></span>
+				AgroCulture is e-commerce trading platform for grains & grocerries...
+			</p>
+			<div class="footer-icons">
+				<a href="https://www.facebook.com/thejaswi.srinivasa/"><i style="margin-left: 0;margin-top:5px;" class="fa fa-facebook"></i></a>
+				<a href="https://www.instagram.com/_notvoid__/"><i style="margin-left: 0;margin-top:5px" class="fa fa-instagram"></i></a>
+				<a href="https://www.youtube.com/@DefinitelyNotVoid"><i style="margin-left: 0;margin-top:5px" class="fa fa-youtube"></i></a>
+			</div>
+		</div>
+
+	</footer>
+
+    </body>
+    </html>
